a05b1b1660742bec2dd31cdfaed11d1aa5c23dcb
nirjal <bistanirjal978@gmail.com>
2024-03-08 15:51:40 +0545

    singleplayer code

--- singlePlayer.java
+++ singlePlayer.java
@@ -1,5 +1,21 @@
-class singlePlayer{
-    public void mySinglePlayer(){
-           
+class singlePlayer {
+
+  public void structure() {
+    int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    // display matrix using for loops
+    // outer loop for row
+    for (int row = -0; row < matrix.length; row++) {
+      // inner loop for column
+      for (int column = 0; column < matrix[row].length; column++) {
+        System.out.print(matrix[row][column] + " ");
+      }
+      System.out.println(); // new line
     }
+  }
+
+  public void mySinglePlayer() {
+    structure();
+
+  }
 }
