7356e50ed07125b3338dc9156c09d4291f2f7f9d
nirjal <bistanirjal978@gmail.com>
2024-03-13 12:13:14 +0545

    printingBoard src

--- printBoard.java
+++ printBoard.java
@@ -1,18 +1,18 @@
 public class printBoard {
-    public void printBrd() {
-        System.out.println("|---|---|---|");
-        String[] board = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-        System.out.println("| " + board[0] + " | "
-                + board[1] + " | " + board[2]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                + board[4] + " | " + board[5]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                + board[7] + " | " + board[8]
-                + " |");
-        System.out.println("|---|---|---|");
-    }
+        public void printBrd() {
+                System.out.println("|---|---|---|");
+                String[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+                System.out.println("| " + board[0] + " | "
+                                + board[1] + " | " + board[2]
+                                + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[3] + " | "
+                                + board[4] + " | " + board[5]
+                                + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[6] + " | "
+                                + board[7] + " | " + board[8]
+                                + " |");
+                System.out.println("|---|---|---|");
+        }
 }
